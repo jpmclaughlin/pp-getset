8a8efa3d616286efae59bbf38132f9b99698cb86
jpmclaughlin <jasonpmclaughlin@gmail.com>
2014-08-12 16:19:08 -0500

    update devUpdate to force latest

--- tasks/options/devUpdate.js
+++ tasks/options/devUpdate.js
@@ -1,8 +1,15 @@
 module.exports = {
   main: {
     options: {
-      reportUpdated: false, // Report updated dependencies: 'false' | 'true'
-      updateType   : "force" // 'force'|'report'|'prompt'
+        updateType: 'force', // 'force'|'report'|'prompt'
+        reportUpdated: false, // don't report up-to-date packages
+        semver: false, // update to latese, irrespective of semver
+        packages: {
+            devDependencies: true, // only check for devDependencies
+            dependencies: false
+        },
+        packageJson: null, // use matchdep default findup to locate package.json
+        reportOnlyPkgs: [] // use updateType action on all packages
     }
   }
 }
